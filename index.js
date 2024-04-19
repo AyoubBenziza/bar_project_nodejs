56fc47a23da130faedea0313d58729fb9d089c5f
AyoubBenziza <ayoub.cbenziza@gmail.com>
2024-04-19 11:56:22 +0200

    add: chemin de routes

--- index.js
+++ index.js
@@ -1,14 +1,26 @@
-const express = require('express');
+// Imports
+const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
 require("dotenv").config();
 
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
-app.get('/', (req,res) => {
-    res.send('<h1>Bar Project</h1>');
-})
+// Librairies de routes
+const barRouter = require("./routers/barRouter");
+const biereRouter = require("./routers/biereRouter");
+const commandeRouter = require("./routers/commandeRouter");
+
+app.get("/", (req, res) => {
+  res.send("<h1>Bar Project</h1>");
+});
+
+// Routes
+app.use("/bar", barRouter);
+app.use("/biere", biereRouter);
+app.use("/commande", commandeRouter);
 
 app.listen(process.env.PORT, () => {
-    console.log(`App Running on http://localhost:${process.env.PORT}`);
+  console.log(`App Running on http://localhost:${process.env.PORT}`);
 });
