ee60abff1ca2434f4df64fb9a3dffe78073795da
Noémie Letenneur <n.letenneur@gmail.com>
2024-04-19 12:38:13 +0200

    correction barRouter

--- routers/barRouter.js
+++ routers/barRouter.js
@@ -1,16 +1,21 @@
-const {getBars,getBarProfil,addBar,editbar,deleteBar} = require("../controllers/barsController")
+const {
+  getBars,
+  getBarProfil,
+  addBar,
+  editbar,
+  deleteBar,
+} = require("../controllers/barsController");
 
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const controller = require('../controllers/barsController')
+const controller = require("../controllers/barsController");
 
+router.get("/", getBars);
+router.get("/:id_bar", getBarProfil);
 
-router.get('/',getBars)
-router.get('/:id_bar',getBarProfil ) 
-
-router.post('/',addBar)
-router.put('/:id_bar',editbar)
-router.delete('/:id_bar',deleteBar)
+router.post("/", addBar);
+router.put("/:id_bar", editbar);
+router.delete("/:id_bar", deleteBar);
 router.get("/:id_bar/biere", getAllBeersFromBar);
 
-module.exports = router
+module.exports = router;
