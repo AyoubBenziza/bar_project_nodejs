2ec63a73429e93207939ec8a09f14e0fc3f1448a
LucasDRT86 <lucasdorat2@gmail.com>
2024-04-19 15:36:43 +0200

    modif model bar

--- models/Bar.js
+++ models/Bar.js
@@ -4,10 +4,15 @@ const db = require('../config/database');
 const Bar = db.define('bar', 
 {
     id : { type: sequelize.INTEGER, primaryKey: true, autoIncrement: true },
-    name: { type: sequelize.STRING  },
+    name: { type: sequelize.STRING, unique: true},
     tel: { type: sequelize.STRING  },
     email: { type: sequelize.STRING },
     description: { type: sequelize.TEXT }
 });
+
+Bar.hasMany(Biere,  {
+    onDelete: "CASCADE",
+    onUpdate: "CASCADE",
+  })
 
 module.exports = Bar;
