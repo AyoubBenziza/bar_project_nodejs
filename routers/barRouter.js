b2d00fb6509265b96e0289724204024ee28bf24d
LucasDRT86 <lucasdorat2@gmail.com>
2024-04-19 14:06:46 +0200

    commentaire router bar

--- routers/barRouter.js
+++ routers/barRouter.js
@@ -10,12 +10,22 @@ const express = require("express");
 const router = express.Router();
 const controller = require("../controllers/barsController");
 
+// Récupérer liste des bars
 router.get("/", getBars);
+
+// Récupérer le profil d'un bar
 router.get("/:id_bar", getBarProfil);
 
+// Ajouter un bar
 router.post("/", addBar);
+
+// Modifier un bar
 router.put("/:id_bar", editbar);
+
+// Suprimer un bar
 router.delete("/:id_bar", deleteBar);
+
+// Récupérer la liste des biere d'un bières d'un bar
 router.get("/:id_bar/biere", getAllBeersFromBar);
 
 module.exports = router;
