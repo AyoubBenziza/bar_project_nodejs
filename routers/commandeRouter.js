e8e4b0563f7b5b208b7fdb7914bc1f4253aacc37
AyoubBenziza <ayoub.cbenziza@gmail.com>
2024-04-22 18:12:50 +0200

    add: Commande Validator

--- routers/commandeRouter.js
+++ routers/commandeRouter.js
@@ -9,14 +9,40 @@ const {
 } = require("../controllers/commandeController");
 const router = express.Router();
 
+// Validators
+const validate = require("../validateurs/validate");
+const {
+  validateCommandId,
+  validateCommandBody,
+} = require("../validateurs/commandeValidator");
+const { validateIdBeer } = require("../validateurs/biereValidator");
+
 // Commande
-router.get("/:idCommand", getCommand);
-router.put("/:idCommand", updateCommand);
-router.delete("/:idCommand", deleteCommand);
+router.get("/:idCommand", validateCommandId, validate, getCommand);
+router.put(
+  "/:idCommand",
+  validateCommandId,
+  validateCommandBody,
+  validate,
+  updateCommand
+);
+router.delete("/:idCommand", validateCommandId, validate, deleteCommand);
 
 // Biere_commande
-router.post("/:idCommand/biere/:idBeer", addBeer);
-router.delete(":idCommand/biere/:idBeer", deleteBeer);
+router.post(
+  "/:idCommand/biere/:idBiere",
+  validateCommandId,
+  validateIdBeer,
+  validate,
+  addBeer
+);
+router.delete(
+  ":idCommand/biere/:idBiere",
+  validateCommandId,
+  validateIdBeer,
+  validate,
+  deleteBeer
+);
 
 // Exports
 module.exports = router;
