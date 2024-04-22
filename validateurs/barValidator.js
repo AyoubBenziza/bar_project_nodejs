fc4d4efe06111bcfb6908ffabbb03efb3991ff2f
LucasDRT86 <lucasdorat2@gmail.com>
2024-04-22 17:27:48 +0200

    barValidator fix

--- validateurs/barValidator.js
+++ validateurs/barValidator.js
@@ -7,8 +7,8 @@ const validateBarBody = [
   body("tel")
     .isMobilePhone({ local: fr - FR })
     .withMessage(`Le numéro de téléphone doit être au format: 01 02 03 04 04`),
-  body("email").isEmail(),
-  body("description").isString().withMessage("Name must be a string"),
+  body("email").isEmail().isString().withMessage("Email must be a string"),
+  body("description").isString().withMessage("Description must be a string"),
 ];
 
 module.exports = { validateIdBar, validateBarBody };
