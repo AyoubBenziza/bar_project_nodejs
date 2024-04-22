c4b0532a8bc0be310dd3607143810dc39b986054
Noémie Letenneur <n.letenneur@gmail.com>
2024-04-22 16:06:47 +0200

    Correction router Bière + début validateur bière

--- validateurs/biereValidator.js
+++ validateurs/biereValidator.js
@@ -1,4 +1,10 @@
-const validateIdBeer = "";
-const validateBodyBeer = "";
+const { body, param, validationResult } = require("express-validator");
+
+const validateIdBeer = [param("idBiere").notEmpty().isNumeric()];
+const validateBodyBeer = [
+  body("name").notEmpty().isString(),
+  body("degree").isFloat,
+  body("degree").isFloat,
+];
 
 module.exports = { validateBodyBeer, validateIdBeer };
