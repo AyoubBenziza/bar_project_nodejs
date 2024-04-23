3939402667fcd1020b065ada38c51ce8698bc18d
LucasDRT86 <lucasdorat2@gmail.com>
2024-04-23 09:39:51 +0200

    Validator fix

--- validateurs/barValidator.js
+++ validateurs/barValidator.js
@@ -1,11 +1,11 @@
-const { param } = require("express-validator");
+const { param, body } = require("express-validator");
 
 const validateIdBar = [param("idBar").notEmpty().isNumeric()];
 
 const validateBarBody = [
   body("name").notEmpty().isString().withMessage("Name must be a string"),
   body("tel")
-    .isMobilePhone({ local: fr - FR })
+    .isMobilePhone({ local: "fr - FR" })
     .withMessage(`Le numéro de téléphone doit être au format: 01 02 03 04 04`),
   body("email").isEmail().isString().withMessage("Email must be a string"),
   body("description").isString().withMessage("Description must be a string"),
