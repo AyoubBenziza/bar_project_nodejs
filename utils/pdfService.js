ca8993e5ef499475c03353529d9a0151c5a309e2
AyoubBenziza <ayoub.cbenziza@gmail.com>
2024-04-24 16:46:57 +0200

    fix: createPDF remove console.log()

--- utils/pdfService.js
+++ utils/pdfService.js
@@ -1,19 +1,27 @@
 const PDFDocument = require("pdfkit");
 
+// Générateur de fichier PDF
 function createPDF(dataCallback, endCallback, data) {
   const commande = data;
-  console.log(`Commande:`, commande);
   const doc = new PDFDocument();
   doc.on("data", dataCallback);
   doc.on("end", endCallback);
+
+  // Variable de position y(vertical) dans le fichier
   let y = 65;
+
+  // HR
   doc.strokeColor("#aaaaaa").lineWidth(1).moveTo(50, y).lineTo(550, y).stroke();
+
+  // Ecriture des données
   for (let property in commande) {
     doc.fontSize(18).text(`${property}: ${commande[property]}`);
-    console.log(`${commande[property]}`);
     y += 22;
   }
+
   doc.strokeColor("#aaaaaa").lineWidth(1).moveTo(50, y).lineTo(550, y).stroke();
+
+  // Fin d'écriture du fichier
   doc.end();
 }
 
